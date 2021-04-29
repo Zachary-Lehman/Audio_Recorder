036e5058213e1e418cf9b618701c3bc09348bb57
Zachary-Lehman <lehmz719@hsd.k12.or.us>
2021-04-28 18:07:22 -0700

    Actually Final code

--- Audio_Recorder.py
+++ Audio_Recorder.py
@@ -7,7 +7,7 @@ import keyboard
 CHUNK = 1024
 #Sets audio format, in this case 16 bit Integer.
 FORMAT = pyaudio.paInt16
-#Sets number of channels (1 = Mono 2 = Stereo)
+#Sets number of channels (1 = Mono 2 = Stereo).
 CHANNELS = 2
 #Sample Rate, the amount of times per second the waveform is sampled.
 RATE = 44100
@@ -15,22 +15,22 @@ RATE = 44100
 WAVE_OUTPUT_FILENAME = "output.wav"
  
 #Writes data from the list 'frames' to a WAV file. 
-def make_wav_file():
-    #Creates new wave file with the name 'WAVE_OUTPUT_FILENAME'
+def write_frames_to_wav_file():
+    #Creates new wave file with the name 'WAVE_OUTPUT_FILENAME'.
     wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
     #Sets amount of audio channels to 'CHANNELS'
     wf.setnchannels(CHANNELS)
     #Sets bit depth using 'FORMAT'
     wf.setsampwidth(p.get_sample_size(FORMAT))
-    #Sets amount of samplings per second using 'RATE'
+    #Sets amount of samplings per second using 'RATE'.
     wf.setframerate(RATE)
-    #Conjugates the data obtained from the recording and writes them to the WAV file using 'frames'
+    #Conjugates the data obtained from the recording and writes them to the WAV file using 'frames'.
     wf.writeframes(b''.join(frames))
-    #Closes WAV file
+    #Closes WAV file.
     wf.close()  
  
 #Writes the list 'frames' to a txt file for debugging.
-def write_frames_to_txt():
+def write_frames_to_txt_file():
     results = open("results.txt", "w")
     results.write("This is The data for the wave file in bytes")
     results.close
@@ -40,14 +40,12 @@ def write_frames_to_txt():
  
 #Sets up event to check if the s key is being pressed, if it exits the while loop.
 is_recording = False
-if is_recording == False:
-    #Tell user what key start recording
-    print("Press s to start recording")
-    while is_recording == False:
-        if keyboard.is_pressed('s'):
-            is_recording = True
+print("Press s to start recording")
+while is_recording == False:
+    if keyboard.is_pressed('s'):
+        is_recording = True
  
-#Sets up an instance of pyaudio
+#Sets up an instance of pyaudio.
 p = pyaudio.PyAudio()
 #Opens audio stream.
 stream = p.open(format=FORMAT,
@@ -76,15 +74,20 @@ if is_recording == True:
 #Tells user that recording has stopped.
 print("* done recording")
  
- 
-#Ends recording, terminates instance of pyaudio
+
+#Ends recording, terminates instance of pyaudio.
 stream.stop_stream()
 stream.close()
 p.terminate()
- 
-#Creates output files
-make_wav_file()
-write_frames_to_txt()
- 
- 
+
+#Asks if the user would also like to make a TXT file for debugging.
+print("Make TXT file from recording for debug? (Y/N)")
+while True:
+    if keyboard.is_pressed('y'):
+        write_frames_to_txt_file()
+        break  
+    elif keyboard.is_pressed('n'): 
+        break
+                                   
+write_frames_to_wav_file()
 
